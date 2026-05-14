10e0b94faf32e91d0b887321d3d2a0be65ea4292
Felipe Lino <felipegadelha2004@gmail.com>
2026-05-13 22:49:07 -0300

    fix: adding the correct variable to the email

--- src/mail/providers/smtp-mail.provider.ts
+++ src/mail/providers/smtp-mail.provider.ts
@@ -5,12 +5,16 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class SmtpMailProvider implements MailProvider {
   constructor(private readonly mailerService: MailerService) {}
+
   async sendRecoveryPasswordEmail(to: string, token: string): Promise<any> {
     return this.mailerService.sendMail({
       to,
       subject: 'Recover Password',
       template: 'recovery-password',
-      context: { token },
+      context: {
+        token,
+        frontend_url: process.env.FRONTEND_URL,
+      },
     });
   }
 }
